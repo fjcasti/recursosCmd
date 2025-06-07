92c04267d020224bb85b1d76cbef7d40b686917d
Casti <fjcasti@hotmail.com>
2025-06-07 14:13:49 +0200

    -

--- copiaSeguridad.bat
+++ copiaSeguridad.bat
@@ -8,12 +8,17 @@ setlocal ENABLEDELAYEDEXPANSION
 rem [x] sin parámetros mostar la ayuda NO. hace una copia y punto.
 rem [x] emitir un pequeño mensaje y hablar del parámetro de ayuda
 rem [x] añadir /h y /? como parámetros para mostar la ayuda.
-REM [ ] parámetro para funcionar en modo silencioso.
-rem [ ] que hacer si se seleccionan mostar configuración y modo silencioso
-rem [ ] si no existe el fichero copiaSeguridad.bat y no se proporciona como parámetro  mostar error / ayuda
-rem [ ] opción que muestre la configuración
-rem [ ] comprobar si un dato dado es fichero o directorio
-rem [ ] verificar la configuración para comprobar el correcto funcionamiento.
+REM [x] parámetro para funcionar en modo silencioso.
+rem [x] opción que muestre la configuración
+rem [x] que hacer si se seleccionan mostar configuración y modo silencioso
+rem [x] si no existe el fichero copiaSeguridad.bat y no se proporciona como parámetro  mostar error / ayuda
+rem [x] verificar la configuración para comprobar el correcto funcionamiento.
+rem [ ] añadir parámetro con un fichero de datos diferente al de por defecto.
+rem [ ] añadir parámetro con una ubicación destino diferente
+rem [ ] añadir parámetro para cambiar el nombre de la copia de seguridad.
+rem [ ] Copiar el fichero de datos de outlook
+rem [ ] Duplicar la copia en una segunda ubicación.
+
 
 set VERSION=0.1
 set ahora=%date:~6,4%%date:~3,2%%date:~0,2%%time:~0,2%%time:~3,2%%time:~6,2%
@@ -21,15 +26,13 @@ set ahora=%ahora: =0%
 rem aquí se hace la compresión
 set RUTACOPIA=.\
 set FICHCOPIA=copiaSeguridad%ahora%.7z
-set outlook=NO
+SET FICHERO_DATOS=copiaSeguridad.dat
 rem empaquetador. Este guión usa 7z.exe
 set COMPRESOR=c:\Program Files\7-Zip\7z.exe
-set FICHERO_DATOS=
 SET VERCONFIGURACION=FALSE
 SET SILENCIOSO=FALSE
-
-
-if exist copiaSeguridad.dat SET FICHERO_DATOS=copiaSeguridad.dat
+set TODO_BIEN=TRUE
+set outlook=NO
 
 
 rem vericar los parámetros pasados
@@ -44,6 +47,7 @@ IF /I "%~1"=="/config" (
 )
 IF /I "%~1"=="/S" (
     set SILENCIOSO=TRUE
+    set VERCONFIGURACION=FALSE
     SHIFT 
     GOTO :bucleParametros
 )
@@ -58,18 +62,40 @@ goto :bucleParametros
     IF %SILENCIOSO% == FALSE (
         echo.
         call :muestra_nombre
-        echo Realizando copia.  /h o /^? para mas información
+        echo     Realizando copia.  /h o /^? muestra opciones disponibles.
     )
 
 if %VERCONFIGURACION%==FALSE GOTO :hacerCopia
-    echo [ ] Mostrar configuración
+    echo [ ] Configuración:
+    echo [ ]     Compresor:           %COMPRESOR%d
+    ECHO [ ]     Fichero de datos:    %FICHERO_DATOS%
+    echo [ ]     Copia de seguridad:  %RUTACOPIA%%FICHCOPIA%
 
 :hacerCopia
 
 IF %SILENCIOSO% == FALSE (
-    echo [ ] Leyendo  "%FICHERO_DATOS%"
-    ECHO [ ] Creando una copia: %RUTACOPIA%%FICHCOPIA%
+    if %VERCONFIGURACION%==FALSE (
+        ECHO [ ] Fichero de datos:    %FICHERO_DATOS%
+        echo [ ] Copia de seguridad:  %RUTACOPIA%%FICHCOPIA%
+    )
 )
+
+REM verificando configuración
+
+IF NOT EXIST "%COMPRESOR%" (
+    ECHO [E] No encuentro el compresor: %COMPRESOR%
+    SET TODO_BIEN=FALSE
+)
+if NOT exist "%FICHERO_DATOS%" (
+    echo [E] No encuentro fichero de datos: %FICHERO_DATOS%. 
+    SET TODO_BIEN=FALSE
+)
+IF NOT EXIST "%RUTACOPIA%" (
+    echo [E] No encuentro la ruta destino: %RUTACOPIA%
+    set TODO_BIEN=FALSE
+)
+IF %TODO_BIEN% == FALSE SET SILENCIOSO=FALSE & GOTO :salir
+
 
 REM tasklist | find "OUTLOOK.EXE" > nul
 REM if %errorlevel% == 0  ( 
@@ -78,10 +104,10 @@ REM 	set outlook=SI
 REM 	taskkill /F /IM OUTLOOK.EXE > NUL
 REM 	WAITFOR /T 1 CASTICOPIASEGURIDAD 2> NUL 
 REM )
-
-for /F "tokens=* EOL=#" %%X in (copiaSeguridad.dat) do (
+IF %SILENCIOSO% == FALSE echo [=] Procesando datos:
+for /F "tokens=* EOL=#" %%X in (%FICHERO_DATOS%) do (
 	set elto=%%X
-    IF %SILENCIOSO% == FALSE echo [=] Elto: !elto!
+    IF %SILENCIOSO% == FALSE echo [-]     !elto!
     "%COMPRESOR%" a -r -bso0 -bsp0 "%RUTACOPIA%%FICHCOPIA%" !elto!
 
 REM 	if exist !elto! (
@@ -110,8 +136,10 @@ goto :salir
     echo  lee una lista de ficheros y/o carpetas los comprime y copia.
     echo  Modo de Uso:
     echo     CopiaSeguridad  [/config] [/h ^| /^?] [/s]
-    echo             /config        muestra la configuración de la aplicación.
+    echo             /config        muestra la configuración de la aplicación. 
+    echo                            No aplica con la opción /s
     echo             /h ^| /^?        muestra la ayuda y termina.
+    echo                            No aplican más opciones.
     echo             /s             modo silencioso. Suprime la emisión de mensajes.
     goto :fin
     
