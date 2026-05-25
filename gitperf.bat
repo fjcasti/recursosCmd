317ded5a6548cd1347164c2c76f05d3b39978843
fjcasti <fjcasti@hotmail.com>
2026-05-25 10:35:13 +0200

    Auto: gitperf.bat

--- gitperf.bat
+++ gitperf.bat
@@ -1,9 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
 
+set "_n=0"
 for /f "usebackq delims=" %%f in (`dir /b "%USERPROFILE%\.gitconfig.*" 2^>nul`) do (
     set "_ext=%%~xf"
-    if "!_ext!" neq "" if /i "!_ext:~1!" neq "gitconfig" echo !_ext:~1!
+    if "!_ext!" neq "" if /i "!_ext:~1!" neq "gitconfig" (
+        set /a "_n+=1"
+        echo !_n!. !_ext:~1!
+    )
 )
 
 endlocal
